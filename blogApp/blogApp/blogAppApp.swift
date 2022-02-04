54c2d0dd9185ee8c502902989b050f6bc632bd06
Wesley Brito <wbernardes@ciandt.com>
2022-02-04 16:47:59 -0300

    initial setup of project

---
+++ blogApp/blogApp/blogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  blogAppApp.swift
+//  blogApp
+//
+//  Created by Wesley Brito Bernardes on 04/02/22.
+//
+
+import SwiftUI
+
+@main
+struct blogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
